aa03b8728231ce63c79deed4f4ea1675f78d4f32
tuanzi <tuanzi@2dfire.com>
2016-09-18 12:50:57 +0800

    test  use search-center

---
+++ src/test/java/com/shaw/test/SearchTest.java
@@ -0,0 +1,44 @@
+package com.shaw.test;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.shaw.result.Result;
+import com.shaw.service.IBlogSearchService;
+import com.shaw.service.IDmhyDataSearchService;
+import com.shaw.vo.BlogVo;
+import com.shaw.vo.DmhyDataVo;
+
+public class SearchTest extends SpringTestCase {
+
+	@Resource
+	private IDmhyDataSearchService dmhyDataSearchService;
+	@Resource
+	private IBlogSearchService blogSearchService;
+
+	@Test
+	public void testSearchDmhyData() throws Exception {
+		String keyword = "美術社";
+		Result<List<DmhyDataVo>> result = dmhyDataSearchService.searchDmhyData(keyword);
+		if (result != null && result.isSuccess() && result.getModel() != null) {
+			for (DmhyDataVo vo : result.getModel()) {
+				System.out.println(vo.toString());
+			}
+		}
+	}
+
+	@Test
+	public void testSearchBlog() {
+		String keyword = "spring";
+		Result<List<BlogVo>> result = blogSearchService.searchBlog(keyword);
+		if (result != null && result.isSuccess() && result.getModel() != null) {
+			for (BlogVo vo : result.getModel()) {
+				System.out.println(vo.getTitle());
+			}
+		}
+	}
+
+}
--- src/test/java/com/shaw/test/SpringTestCase.java
+++ src/test/java/com/shaw/test/SpringTestCase.java
@@ -5,7 +5,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@ContextConfiguration(locations = { "classpath:applicationContext.xml", "classpath:test.xml" })
+@ContextConfiguration(locations = { "classpath:applicationContext.xml", "classpath:test.xml"})
 @RunWith(SpringJUnit4ClassRunner.class)
 public class SpringTestCase extends AbstractJUnit4SpringContextTests {
 
